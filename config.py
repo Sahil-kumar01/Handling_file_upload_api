101e891a7998e05cf9bfb88074693f789b5d15d5
Sahil-kumar01 <sahilkumar.ktp16@gmail.com>
2024-08-05 10:10:17 +0530

    "Updated subproject commit to indicate dirty state"

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 UPLOAD_DIR = 'static/uploads/'
 CONVERTED_DIR = 'static/converted/'
-mongo_url = 'mongodb+srv://sahilkumarktp16:S%40hil2003@mongoy.q4auz7c.mongodb.net/files'
+mongo_url = 'your_mongo_db_link'
 
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 os.makedirs(CONVERTED_DIR, exist_ok=True)
